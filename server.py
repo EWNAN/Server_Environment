5889e9b0613baa9d8e104ff34ad7a18f364c63a2
EWNAN <ebennortey14@gmail.com>
2023-02-08 15:23:31 +0000

    Adding extre resource files

--- server.py
+++ server.py
@@ -1,10 +1,38 @@
 #gunicorn
-def render_template(template_name = 'index.html'):
-        return "<h1>Hello World</h1>"
+def render_template(template_name = 'index.html', context={}):
+    html_str = ""
+    with open(template_name, 'r') as f:
+        html_str = f.read()
+        html_str = html_str.format(**context)
+        return html_str
+
+
+def home(environ):
+    return render_template(
+        template_name= "index.html",
+        context= {}
+    )
+
+def Contact_us(environ):
+    return render_template(
+        template_name= "contact.html",
+        context= {}
+    )    
 
 def app(environ, start_response):
+        path = environ.get("PATH_INFO")
+        if path.endswith("/"):
+            path = path[:-1]
+        if path == "/":
+            data = home(environ)
+        elif path == "/contact":
+            data = Contact_us(environ)    
+        else:
+            data = render_template(template_name = '404.html', path=path)
+        #for k, v in environ.items():
+       #     print(k,v)
        # data = "Hello World!!!!!"
-        data = render_template()
+       # data = render_template()
         data = data.encode("utf-8")
         
         start_response(
