c8ad3c8d777f23120755785b39653aee41733200
EWNAN <ebennortey14@gmail.com>
2023-02-08 12:20:00 +0000

    Updated server

--- server.py
+++ server.py
@@ -1,15 +1,16 @@
 #gunicorn
 def render_template(template_name = 'index.html'):
-        return "Hello World"
+        return "<h1>Hello World</h1>"
 
 def app(environ, start_response):
-        data = "Hello World!"
+       # data = "Hello World!!!!!"
+        data = render_template()
         data = data.encode("utf-8")
-        #data = render_template()
+        
         start_response(
             f"200 OK", [
-                ("Content-type", "text/plain"), # for the HTML style to work effectively the text style has to be HTML not plain
-                ("Content-length", str(len("data")))
+                ("Content-type", "text/HTML"), # for the HTML style to work effectively the text style has to be HTML not plain
+                ("Content-length", str(len(data)))
             ]
         )
         return iter([data])
